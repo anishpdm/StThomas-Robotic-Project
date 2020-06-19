326a66a2634ad9fb123a85f22cf169b54afa58fb
anishpdm <anish.vilayil.s2gmail.com>
2020-06-19 12:26:41 +0530

    a

--- app/src/main/java/com/anish/robotic_stthomas/RoverControl.java
+++ app/src/main/java/com/anish/robotic_stthomas/RoverControl.java
@@ -3,12 +3,205 @@ package com.anish.robotic_stthomas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+import org.eclipse.paho.android.service.MqttAndroidClient;
+import org.eclipse.paho.client.mqttv3.DisconnectedBufferOptions;
+import org.eclipse.paho.client.mqttv3.IMqttActionListener;
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.IMqttMessageListener;
+import org.eclipse.paho.client.mqttv3.IMqttToken;
+import org.eclipse.paho.client.mqttv3.MqttCallbackExtended;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
 public class RoverControl extends AppCompatActivity {
-
+    final String serverUri = "tcp://3.128.122.63:1883";
+    String clientId = "ExampleAndroidClient";
+    final String subscriptionTopic = "Topic";
+    final String publishTopic = "Topic";
+    final String publishMessagea= "a";
+    final String publishMessageb = "b";
+    final String publishMessagec = "c";
+    final String publishMessaged = "d";
+    final String publishMessagee = "e";
+    private MqttAndroidClient mqttAndroidClient;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rover_control);
+        Button right = (Button) findViewById(R.id.right);
+        Button left = (Button) findViewById(R.id.left);
+        Button straight = (Button) findViewById(R.id.straight);
+        Button reverse = (Button) findViewById(R.id.reverse);
+        Button stop = (Button) findViewById(R.id.stop);
+
+        right.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                publishMessage(publishMessagea);
+                Toast.makeText(getApplicationContext(),"Turn Right",Toast.LENGTH_LONG).show();
+            }
+        });
+
+        left.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                publishMessage(publishMessageb);
+                Toast.makeText(getApplicationContext(),"Turn Left",Toast.LENGTH_LONG).show();
+            }
+        });
+        straight.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                publishMessage(publishMessagec);
+                Toast.makeText(getApplicationContext(),"Straignt",Toast.LENGTH_LONG).show();
+            }
+        });
+
+        reverse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                publishMessage(publishMessaged);
+                Toast.makeText(getApplicationContext(),"Reverse",Toast.LENGTH_LONG).show();
+            }
+        });
+
+        stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                publishMessage(publishMessagee);
+                Toast.makeText(getApplicationContext(),"Stop",Toast.LENGTH_LONG).show();
+            }
+        });
+
+
+
+
+
+
+        clientId = clientId + System.currentTimeMillis();
+
+        mqttAndroidClient = new MqttAndroidClient(getApplicationContext(), serverUri, clientId);
+        mqttAndroidClient.setCallback(new MqttCallbackExtended() {
+            @Override
+            public void connectComplete(boolean reconnect, String serverURI) {
+
+                if (reconnect) {
+                    addToHistory("Reconnected to : " + serverURI);
+                    // Because Clean Session is true, we need to re-subscribe
+                    subscribeToTopic();
+                } else {
+                    addToHistory("Connected to: " + serverURI);
+                }
+            }
+
+            @Override
+            public void connectionLost(Throwable cause) {
+                addToHistory("The Connection was lost.");
+            }
+
+            @Override
+            public void messageArrived(String topic, MqttMessage message) throws Exception {
+                addToHistory("Incoming message: " + new String(message.getPayload()));
+            }
+
+            @Override
+            public void deliveryComplete(IMqttDeliveryToken token) {
+
+            }
+        });
+
+        MqttConnectOptions mqttConnectOptions = new MqttConnectOptions();
+        mqttConnectOptions.setAutomaticReconnect(true);
+        mqttConnectOptions.setCleanSession(false);
+
+
+
+
+
+
+
+        try {
+            //addToHistory("Connecting to " + serverUri);
+            mqttAndroidClient.connect(mqttConnectOptions, null, new IMqttActionListener() {
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+                    DisconnectedBufferOptions disconnectedBufferOptions = new DisconnectedBufferOptions();
+                    disconnectedBufferOptions.setBufferEnabled(true);
+                    disconnectedBufferOptions.setBufferSize(100);
+                    disconnectedBufferOptions.setPersistBuffer(false);
+                    disconnectedBufferOptions.setDeleteOldestMessages(false);
+                    mqttAndroidClient.setBufferOpts(disconnectedBufferOptions);
+                    subscribeToTopic();
+                }
+
+                @Override
+                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                    addToHistory("Failed to connect to: " + serverUri);
+                }
+            });
+
+
+        } catch (MqttException ex){
+            ex.printStackTrace();
+        }
+
+    }
+
+
+    private void addToHistory(String mainText){
+        System.out.println("LOG: " + mainText);
+
+
+    }
+
+
+    public void subscribeToTopic(){
+        try {
+            mqttAndroidClient.subscribe(subscriptionTopic, 0, null, new IMqttActionListener() {
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+                    addToHistory("Subscribed!");
+                }
+
+                @Override
+                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                    addToHistory("Failed to subscribe");
+                }
+            });
+
+            // THIS DOES NOT WORK!
+            mqttAndroidClient.subscribe(subscriptionTopic, 0, new IMqttMessageListener() {
+                @Override
+                public void messageArrived(String topic, MqttMessage message) throws Exception {
+                    // message Arrived!
+                    System.out.println("Message: " + topic + " : " + new String(message.getPayload()));
+                }
+            });
+
+        } catch (MqttException ex){
+            System.err.println("Exception whilst subscribing");
+            ex.printStackTrace();
+        }
+    }
+
+    public void publishMessage(String m){
+
+        try {
+            MqttMessage message = new MqttMessage();
+            message.setPayload(m.getBytes());
+            mqttAndroidClient.publish(publishTopic, message);
+            addToHistory("Message Published");
+            if(!mqttAndroidClient.isConnected()){
+                // addToHistory(mqttAndroidClient.getBufferedMessageCount() + " messages in buffer.");
+            }
+        } catch (MqttException e) {
+            System.err.println("Error Publishing: " + e.getMessage());
+            e.printStackTrace();
+        }
     }
 }
--- app/src/main/java/com/anish/robotic_stthomas/ViewHumActivity.java
+++ app/src/main/java/com/anish/robotic_stthomas/ViewHumActivity.java
@@ -3,12 +3,99 @@ package com.anish.robotic_stthomas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class ViewHumActivity extends AppCompatActivity {
-
+    String Api ="http://3.128.122.63/viewall_api.php";
+    TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_hum);
+        tv=(TextView)findViewById(R.id.display);
+        callApi();
+    }
+
+    private void callApi() {
+        Log.d("test", Api);
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, Api,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+
+                            Log.d("test", response);
+
+//                            Toast.makeText(getActivity().getApplicationContext(), "Response From Server " + response, Toast.LENGTH_LONG).show();
+
+                            JSONObject jsonObject = new JSONObject(response);
+
+                            String rslt = jsonObject.getString("status");
+
+                            if(rslt.equals("ok"))
+                            {
+                                String hum = jsonObject.getString("hum");
+                                String temp = jsonObject.getString("temp");
+                                String moist = jsonObject.getString("mosit");
+                                tv.setText(hum);
+
+                            }
+                            else{
+
+                                Toast.makeText(getApplicationContext().getApplicationContext(), "No Data Available ", Toast.LENGTH_LONG).show();
+
+
+                            }
+
+
+
+
+                        } catch (JSONException ob) {
+                            Toast.makeText(getApplicationContext().getApplicationContext(), "Exception  " + ob, Toast.LENGTH_LONG).show();
+                        }
+
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                        Log.d("test", error.toString());
+
+
+                    }
+                }) {
+            protected Map<String, String> getParams() throws AuthFailureError {
+                HashMap<String, String> params = new HashMap<>();
+
+
+
+                return params;
+
+            }
+
+
+        };
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+
     }
 }
